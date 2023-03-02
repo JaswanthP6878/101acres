8c7c7b15afbaab93a85e8fbb6832b881adbf3eda
JaswanthP6878 <klucse2000039034@gmail.com>
2023-03-02 21:30:08 +0530

    added client but need to add code for styling

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import './styles/Navbar.css';
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className='nav-item-container'>
+                <div className='nav-item'>
+                    navItem
+                </div>
+                <div className='nav-item'>
+                    <ul className='nav-item-list'>
+                        <li>list item</li>
+                        <li>list item</li>
+                        <li>list item</li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,14 +1,43 @@
+
+
+// !!!!!! rename the seed.js file to seed.cjs
+
+
 import { PrismaClient } from '@prisma/client'
+import * as bcrypt from "bcrypt";
 const prisma = new PrismaClient()
 async function main() {
-    // already installed bcrypt now just run the hash and add to database.
-}
+
+  // const password = await bcrypt.hash("nicePassword", 4);
+    // to create a user
+      // await prisma.user.create({
+      //   data : {
+      //     id: 3,
+      //     FirstName: "Jaswanth",
+      //     LastName: "raja",
+      //     UserName: "JP68",
+      //     email: "jpinnepu123@gmail.com",
+      //     password,
+      //   },
+      // });
+    // to find a user
+    //console.log(await prisma.user.findMany());
+
+    // to check if a user is passing the form
+    const data = await prisma.user.findFirst({where: {
+      UserName: "JP68",
+    }}); 
+    console.log(data);
+    const match = await bcrypt.compare("nicePassword",data!.password);
+    if (match) {
+      console.log("match found!");
+    } else {
+      console.log("match not found!");
+    }
+    } 
 main()
-
   .then(async () => {
-
     await prisma.$disconnect()
-
   })
 
   .catch(async (e) => {
