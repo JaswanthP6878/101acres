a4cfe8acc6320c6c02b88af90e6dabf1b424ac4c
JaswanthP6878 <klucse2000039034@gmail.com>
2023-03-02 21:36:24 +0530

    trivial updates

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,21 +1,25 @@
-import './styles/Navbar.css';
+// import './styles/Navbar.css';
 
 export default function Navbar() {
     return (
-        <nav className="navbar">
-            <div className='nav-item-container'>
-                <div className='nav-item'>
-                    navItem
-                </div>
-                <div className='nav-item'>
-                    <ul className='nav-item-list'>
-                        <li>list item</li>
-                        <li>list item</li>
-                        <li>list item</li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        // <nav className="navbar">
+        //     <div className='nav-item-container'>
+        //         <div className='nav-item'>
+        //             navItem
+        //         </div>
+        //         <div className='nav-item'>
+        //             <ul className='nav-item-list'>
+        //                 <li>list item</li>
+        //                 <li>list item</li>
+        //                 <li>list item</li>
+        //             </ul>
+        //         </div>
+        //     </div>
+        // </nav>
+
+        <div className="text-3xl">
+                Hello world!
+        </div>
     )
 }
 
