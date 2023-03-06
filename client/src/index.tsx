9585f8158e4f08244d74a9ac9d539d3691fcc972
JaswanthP6878 <klucse2000039034@gmail.com>
2023-03-06 21:06:59 +0530

    added browser router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import LandingPage from './routes/LandingPage';
+import SignUpPage from './routes/SignupPage';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element : <LandingPage />
+  },
+  {
+    path: '/signup',
+    element: <SignUpPage />
+  }
+]
+)
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+  <React.StrictMode>
+    <RouterProvider router={router} />
+    </React.StrictMode>
 );
---
+++ client/src/routes/SignupPage.tsx
@@ -0,0 +1,7 @@
+export default function SignUpPage() {
+    return (
+        <div>
+            <h3>Hello from signup page</h3>
+        </div>
+    )
+}
